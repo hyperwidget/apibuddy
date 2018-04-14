533bd2b1282cdb640e2498c0d27b94f6cd5a1f14
Hyperwidget <rawkamatic@gmail.com>
2018-04-14 10:42:04 -0400

    Remove unneeded everything

--- app.js
+++ app.js
@@ -3,20 +3,6 @@ const fetch = require('node-fetch')
 const util = require('util')
 const app = express()
 
-var userAgent = util.format(
-  'apibuddy/%s (%s)',
-  '0.1',
-  'hunter@hypderwidget.com'
-)
-
-console.log(userAgent)
-
-// app.use(
-//   bodyParser.urlencoded({
-//     // to support URL-encoded bodies
-//     extended: true
-//   })
-// )
 app.use(express.json()) // to support JSON-encoded bodies
 app.use(express.urlencoded())
 app.use(function(req, res, next) {
@@ -33,23 +19,12 @@ app.post('/', async (req, res) => {
   const fetch = require('node-fetch')
   const { url, options } = req.body
 
-  console.log(options, 'OPTIONS')
   const response = await fetch(url, options)
-    .then(res => {
-      //   if (res.ok) {
-      return res
-      //   }
-
-      //   return Promise.reject(
-      //     new Error(`Failed to fetch ${res.url}: ${res.status} ${res.statusText}`)
-      //   )
-    })
+    .then(res => res)
     .then(res => res.json())
     .catch(err => err)
-
-  console.log(response, 'response')
 
   res.json(response).end()
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('APIBUDDY listening on port 3000!'))
